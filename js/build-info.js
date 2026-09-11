4ae92a93c6a53b99b0f02a9cb2bbf96c8a29dd5e
PhyteHQ <ax.gro90@gmail.com>
2026-09-11 11:36:20 +0200

    Prevent sticky recipe controls from overlapping the comparison

--- js/build-info.js
+++ js/build-info.js
@@ -1,6 +1,6 @@
 /* Shared by the page, asset loader and service worker. Bump for each release. */
 globalThis.RHW_BUILD = Object.freeze({
-  revision: '2026-09-11-recipe-compare-1',
+  revision: '2026-09-11-recipe-compare-2',
   repository: 'PhyteHQ/RHW',
   branch: 'main'
 });
